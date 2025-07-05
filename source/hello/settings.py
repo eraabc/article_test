bb3dce6cc2ba7b34801c8e7b2e636b2a96135150
Erbolot <eraabc.main@gmail.com>
2025-07-05 13:05:57 +0600

    изменили настройки бд

--- source/hello/settings.py
+++ source/hello/settings.py
@@ -70,19 +70,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'hello.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'articles_db',
+        'USER': 'postgres',
+        'PASSWORD': 'erbolot2006e',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-
-from webapp.articles_db import ArticlesDB
 from webapp.models import Article
 
 
